96a8757bfc4f2d7af3d1a4fb1c646732af36d83b
Kenny Czadzeck <kennycgit@gmail.com>
2019-02-03 00:02:48 -0800

    remove comment

--- src/spotify/services.js
+++ src/spotify/services.js
@@ -34,10 +34,4 @@ export default class SpotifyService {
     })
   }
 
-  /*
-    http://localhost:3000/spotify-callback?code=AQCcK4hfEqcVTEs3OcUAQRJf4fmEKRdnrtlVBpdLc0JawvMP9Sl9Y4fcqZMnqgxFV9Or__pVJ4bBLDD5cghHpyKJUtdU-XqyczfeM7UZSRfRZ9aNStJdMW09ZVZYazN8BPBeeRyuIc8YAKO9IXIeS0yVki04mmG-WCB6YnK0O3GBIVSM5ctaooS3-GppbRgpf5s6cj_uC7f9xgKgXt89DiHFvvV5v1l0xcbveg3Y-l0EZVsMfZxmYN3KygIkzXnk7hD1tTG4xvBRfdOLQ_BA-uk6Z3YiOcS5omf57fNBpAF5M_oR4PLwSWihQFiYIpY70KWU6qMeBl4w-U8VKwbH71ZCkVIkFGAX
-  */
-
-  
-
 }
